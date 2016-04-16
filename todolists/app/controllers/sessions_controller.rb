12077c37cc99b6570e9b75c885800097ff14e43d
jfcarvalho <jfcarvalho@dcc.ufba.br>
2016-04-15 21:31:31 -0300

    requisito 10 ok

--- todolists/app/controllers/sessions_controller.rb
+++ todolists/app/controllers/sessions_controller.rb
@@ -3,7 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	username = User.find_by(username: params[:user][:username])
+  	password = params[:user][:password]
+
+  	if username && username.authenticate(password)
+  		session[:user_id] = username.id
+  		redirect_to root_path, notice: "Logged in successfully"
+
+  	else
+  		redirect_to login_path, alert: "Invalid username/password combination"	
   end
+end
 
   def destroy
   end
